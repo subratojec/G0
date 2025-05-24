a9cd7622f2bdb041a13935558cac088501bb2933
Subrato Singh <kaatib.singh@gmail.com>
2025-05-24 11:46:03 +0530

    updated few things on Back application

--- bank.go
+++ bank.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("Welcome to the Go BANK")
+	fmt.Println("What do you want to do ?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit")
+	fmt.Println("3. Withdraw")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice:")
+	fmt.Scan(&choice)
+
+	fmt.Println("You entered : ", choice)
 
 }
